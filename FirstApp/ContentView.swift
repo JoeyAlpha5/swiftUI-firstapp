134995961f33254936dabba8b8589afdc774d60d
Jalome Chirwa <click2sure@Jalomes-Air.home>
2022-09-02 19:25:21 +0200

    added UI for war challenge game

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -10,22 +10,80 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
 
+//        VStack{
+//
+//            Image("ImageElement")
+//                .resizable()
+//                .aspectRatio(contentMode:.fit)
+//
+//            Spacer()
+//
+//            Text("Hello, world!")
+//            .padding()
+//            .foregroundColor(Color.red)
+//
+//            HStack{
+//                Text("Another text element")
+//            }
+//        }
+        
         VStack{
-            
             Image("ImageElement")
                 .resizable()
-                .aspectRatio(contentMode:.fit)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200.0)
+                .padding(.top, 10)
+//               add space break to add cards
+            Spacer()
+            Spacer()
+            
+            
+//          stack to display main cards
+            HStack{
+                Spacer()
+                Image("card3")
+                
+                Spacer()
+                
+                Image("card4")
+                Spacer()
+            }
             
             Spacer()
             
-            Text("Hello, world!")
-            .padding()
-            .foregroundColor(Color.red)
-            
+//          display deals image
+            Image("deals")
+            Spacer()
+//           counter view
             HStack{
-                Text("Another text element")
+                Spacer()
+                VStack{
+                    Text("Player")
+                        .foregroundColor(Color.white)
+                        .fontWeight(.bold)
+                    Text("0")
+                        .foregroundColor(Color.white)
+                        .padding()
+                    
+                }
+                Spacer()
+                VStack{
+                    Text("CPU")
+                        .foregroundColor(Color.white)
+                        .fontWeight(.bold)
+                    Text("0")
+                        .foregroundColor(Color.white)
+                        .padding()
+                }
+                Spacer()
             }
+            
+            Spacer()
+            
+            
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
         
 
 
